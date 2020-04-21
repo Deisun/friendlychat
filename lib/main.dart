f03771537a8047fe2343c14ed8a9db54c110fd74
Deisun <rdouma81@gmail.com>
2020-04-21 13:08:30 -0400

    Chat is working now.

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,23 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text("Friendly Chat"),
       ),
-      body: _buildTextComposer(),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+            ),
+        ],
+      )
     );
   }
 
